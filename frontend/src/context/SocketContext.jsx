c6bcbdcd7421dbec57be6a811919a1d5f5e3cc44
Muhammad Anas <anasraza6096@gmail.com>
2024-11-14 00:41:48 +0500

    bug fixing test

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -21,8 +21,7 @@ const SocketContextProvider =({children})=>{
         // console.log("UseEffect Running correctly")
 
         // const socket = io("https://thremedia.vercel.app",{
-        const socket = io("http://localhost:5000",{
-        // const socket = io("/",{
+        const socket = io("/",{
             query:{
                 userId:user?._id,
             },
