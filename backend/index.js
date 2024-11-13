e57d7ac9686f048e2dbbff10998b89c8a37a2109
Muhammad Anas <anasraza6096@gmail.com>
2024-11-14 00:32:38 +0500

    .....

--- backend/index.js
+++ backend/index.js
@@ -37,8 +37,13 @@ const PORT = process.env.PORT || 5000;
 
 
 
-app.use(cors()) // Use this after the variable declaration
-
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME, 
